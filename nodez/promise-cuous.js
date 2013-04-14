ab0c6019e471d4f0ddf49e767d71655afd724574
Jared Hirsch <ohai@6a68.net>
2013-04-13 23:43:22 -0700

    start messin with mocha-as-promised

--- nodez/promise-cuous.js
+++ nodez/promise-cuous.js
@@ -8,4 +8,26 @@ suite('yow', function() {
   test('return a promise', function() {
     return Q.fcall(function() { assert.ok(true) })
   });
+
+  test('wait for several calls', function() {
+    return Q.all([
+      Q.fcall(function() { assert.ok(true) }),
+      Q.fcall(function() { assert.ok('yargg') }),
+      Q.fcall(function() { assert.ok(true) })
+    ]);
+  });
+
+  test('wait for paused call', function(done) {
+    var def = Q.defer();
+    setTimeout(function() {
+      def.resolve();
+    }, 1000);
+    return def.promise;
+  });
+
+  test('chained calls', function(done) {
+    return Q.fcall(function() { assert.ok(true) })
+              .then(Q.fcall(function() { assert.ok(true) }));
+  });
+
 });
